bbce05fc16b68edfecafc6bcd71b7eea55fcb8af
lekhanShetty15 <lekhanshetty2000@gmail.com>
2024-03-17 17:38:03 +0530

    added the next page and added the home button on the nav bar to refresh the page from start

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let slide = document.getElementById("slide");
 let cut = document.getElementById("cut");
-let slidebar =document.getElementById("slidebar")
+let slidebar =document.getElementById("slidebar");
+let homeBtn = document.getElementById("home");
+
+homeBtn.addEventListener("click", () =>{
+    location.reload();
+})
 
 slide.addEventListener("click", ()=>{
     slidebar.style.display="block"
@@ -13,5 +18,13 @@ cut.addEventListener("click", ()=>{
 
 
 
+let bjpBtn = document.getElementById("bjpBtn");
+let ALLparty = document.getElementById("ALLparty");
+let all = document.getElementById("all");
+let bjp = document.getElementById("main2");
 
-
+bjpBtn.addEventListener("click",()=>{
+    all.style.display="none"
+    bjp.style.display="block"
+    
+})
